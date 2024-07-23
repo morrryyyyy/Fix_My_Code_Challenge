99a9f13dee25f7e103c87c114a76dd5fa936ab15
Maryam <morrryyyy21@gmail.com>
2024-07-23 17:17:28 +0100

    Fix my code challenege

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,49 +11,49 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *saved_head;
-    dlistint_t *tmp;
-    unsigned int p;
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
+	unsigned int p;
 
-    if (*head == NULL)
-    {
-        return (-1);
-    }
-    saved_head = *head;
-    p = 0;
-    while (p < index && *head != NULL)
-    {
-        *head = (*head)->next;
-        p++;
-    }
-    if (p != index || *head == NULL)
-    {
-        *head = saved_head;
-        return (-1);
-    }
-    if (0 == index)
-    {
-        tmp = (*head)->next;
-        free(*head);
-        *head = tmp;
-        if (tmp != NULL)
-        {
-            tmp->prev = NULL;
-        }
-    }
-    else
-    {
-        tmp = *head;
-        if (tmp->prev != NULL)
-        {
-            tmp->prev->next = tmp->next;
-        }
-        if (tmp->next != NULL)
-        {
-            tmp->next->prev = tmp->prev;
-        }
-        free(tmp);
-        *head = saved_head;
-    }
-    return (1);
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	saved_head = *head;
+	p = 0;
+	while (p < index && *head != NULL)
+	{
+		*head = (*head)->next;
+		p++;
+	}
+	if (p != index || *head == NULL)
+	{
+		*head = saved_head;
+		return (-1);
+	}
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
+	}
+	else
+	{
+		tmp = *head;
+		if (tmp->prev != NULL)
+		{
+			tmp->prev->next = tmp->next;
+		}
+		if (tmp->next != NULL)
+		{
+			tmp->next->prev = tmp->prev;
+		}
+		free(tmp);
+		*head = saved_head;
+	}
+	return (1);
 }
